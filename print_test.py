bcebc971b2c9fbd98aef09719e47dd2ac884c055
andreibaden <andreibaden@gmail.com>
2023-04-02 08:26:56 +0300

    factorial of a number

--- print_test.py
+++ print_test.py
@@ -1,1 +1,25 @@
-print("Hi, Andrei!")
+# factorial
+
+# def factorial(num):
+#     f = 1
+#     while num > 1:
+#         f *= num
+#         num -= 1
+#     return f
+
+def factorial(num):
+    f = 1
+    for v in range(2, num + 1):
+        f *= v
+    return f
+
+
+def main():
+    num = int(input("Input your number > 0: "))
+    fact = factorial(num)
+    print(f"The factorial of a number {num} is [{fact}]")
+
+
+main()
+
+print(type(5))
